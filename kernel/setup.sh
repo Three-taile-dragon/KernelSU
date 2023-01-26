18a303f157b0c5b10e229a4c39872c26dba9200f
Three-taile-dragon <47767754+Three-taile-dragon@users.noreply.github.com>
2023-01-26 09:01:00 +0800

    Update setup.sh

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -16,7 +16,7 @@ else
      exit 127
 fi
 
-test -d "$GKI_ROOT/KernelSU" || git clone https://github.com/tiann/KernelSU
+test -d "$GKI_ROOT/KernelSU" || git clone https://github.com/ookiineko/KernelSU -b dev
 cd "$GKI_ROOT/KernelSU"
 git stash && git pull
 cd "$GKI_ROOT"
